2b071fc776d46afafdf586e97d5d4f938fd07659
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-08-14 22:20:24 -0300

    MEDIA QUERIES portfolio.css

--- src/sections/portfolio/CategoryButtom.jsx
+++ src/sections/portfolio/CategoryButtom.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const CategoryButtom = ({category, className}) => {
+const CategoryButtom = ({category, className, onChangeCategory}) => {
   return (
-    <button className={className}>{category}</button>
+    <button className={className} onClick={() => onChangeCategory(category)}>{category}</button>
   )
 }
 
--- src/sections/portfolio/ProjectsCategories.jsx
+++ src/sections/portfolio/ProjectsCategories.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import { useState } from 'react'
 import CategoryButtom from './CategoryButtom'
 
-const ProjectsCategories = (categories) => {
+const ProjectsCategories = (categories, onFilterProjects) => {
+    const [activeCategory, setActiveCategory] = useState('all');
+
+    const changeCategoryHandler = (activeCat) => {
+        setActiveCategory(activeCat);
+        onFilterProjects(activeCat);
+    }
+
   return (
     <div className='portfolio__categories'>
         {
             categories.map(category => (
-                <CategoryButtom key={category} category={category} className={`btn cat__btn white`}/>
+                <CategoryButtom key={category} category={category} onChangeCategory={() => 
+                changeCategoryHandler(category)} className={`btn cat__btn ${activeCategory == category ? 
+                'primary' : 'white'}`}/>
             ))
         }
     </div>
