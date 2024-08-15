f5a8738773a8a8e001243925c56db2618b265aef
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-08-14 23:03:49 -0300

    Update Testimonials.jsx

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -11,6 +11,7 @@ import './styles.css';
 import { Pagination } from 'swiper/modules';
 
 import testimonials from './data'
+import Testimonialfrom from './Testimonial';
 import './testimonials.css'
 
 
@@ -31,15 +32,13 @@ const Testimonials = () => {
               modules={[Pagination]}
               className="mySwiper"
             >
-              <SwiperSlide>Slide 1</SwiperSlide>
-              <SwiperSlide>Slide 2</SwiperSlide>
-              <SwiperSlide>Slide 3</SwiperSlide>
-              <SwiperSlide>Slide 4</SwiperSlide>
-              <SwiperSlide>Slide 5</SwiperSlide>
-              <SwiperSlide>Slide 6</SwiperSlide>
-              <SwiperSlide>Slide 7</SwiperSlide>
-              <SwiperSlide>Slide 8</SwiperSlide>
-              <SwiperSlide>Slide 9</SwiperSlide>
+              {
+                testimonials.map(testimonial => (
+                  <SwiperSlide key={testimonial.id}>
+                    <Testimonial/>
+                  </SwiperSlide>
+                ))
+              }
             </Swiper>
           </div>
           </section>
