4538952b4793f4973efcbc86f83ad28b77e95a34
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-08-14 22:53:31 -0300

    Update Testimonials.jsx

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,3 +1,15 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 import './testimonials.css'
 
 const Testimonials = () => {
