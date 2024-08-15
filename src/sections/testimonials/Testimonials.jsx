152f520889fd8374df13d7b23a449e99671545cf
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-08-14 22:57:00 -0300

    Update Testimonials.jsx

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -12,6 +12,7 @@ import { Pagination } from 'swiper/modules';
 
 import './testimonials.css'
 
+
 const Testimonials = () => {
   return (
         <section id="testimonials">
@@ -20,7 +21,25 @@ const Testimonials = () => {
             These are unbiased testmonials from some of my clients
           </p>
           <div className="container">
-
+                <Swiper
+              slidesPerView={3}
+              spaceBetween={30}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              <SwiperSlide>Slide 1</SwiperSlide>
+              <SwiperSlide>Slide 2</SwiperSlide>
+              <SwiperSlide>Slide 3</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 5</SwiperSlide>
+              <SwiperSlide>Slide 6</SwiperSlide>
+              <SwiperSlide>Slide 7</SwiperSlide>
+              <SwiperSlide>Slide 8</SwiperSlide>
+              <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
           </div>
           </section>
     )
