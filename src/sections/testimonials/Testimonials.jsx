9587c75d75e65816704f1c0dc6dd8a271da25001
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-08-16 23:21:44 -0300

    Update Testimonials.jsx

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,9 +1,11 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/autoplay'
 
 import './styles.css';
 
@@ -24,12 +26,17 @@ const Testimonials = () => {
           </p>
           <div className="container">
                 <Swiper
-              slidesPerView={3}
+              slidesPerView={1}
               spaceBetween={30}
+              breakpoints={{
+                601: {slidesPerView: 2},
+                1025: {slidesPerView: 3}
+              }}
               pagination={{
                 clickable: true,
               }}
-              modules={[Pagination]}
+              modules={[Pagination, Autoplay]}
+              autoplay={true}
               className="mySwiper"
             >
               {
