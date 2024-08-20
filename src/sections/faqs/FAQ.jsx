da6aa167e39f557d6a9414c442a01e9d62ac0894
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-08-20 00:09:24 -0300

    Update FAQ.jsx

--- src/sections/faqs/FAQ.jsx
+++ src/sections/faqs/FAQ.jsx
@@ -1,16 +1,16 @@
 import Card from "../../components/Card"
-import {AiOutlinePlus} from 'react-icons/ai'
+import {AiOutlinePlus, AiOutlineMinus} from 'react-icons/ai'
 
 const FAQ = ({faq}) => {
     const [showAnswer, setShowAnswer] = useState(false);
 
 
   return (
-    <Card classname="faq">
+    <Card classname="faq" onClick={() => setShowAnswer(prev => !prev)}>
         <div>
             <h5 lassname="faq__question">{faq.question}</h5>
             <button className='faq__icon'>
-                {<AiOutlinePlus/>}
+                {showAnswer ? <AiOutlineMinus/> : <AiOutlinePlus/>}
             </button>
         </div>
         {showAnswer && <p className="faq__answer">{faq.answer}</p>}
