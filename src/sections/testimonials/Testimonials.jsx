1c3f900b00eb8f488e9c3c6761451ad8f849d330
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-08-21 15:51:15 -0300

    Create contact data.js

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,18 +1,17 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper";
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/autoplay"
-
-// import required modules
-import { Pagination } from "swiper";
-
-
-import testimonials from './data'
-import Testimonial from "./Testimonial";
-import './testimonials.css'
+    // Import Swiper React components
+    import { Swiper, SwiperSlide } from 'swiper/react';
+     
+    // Import Swiper styles
+    import 'swiper/css';
+    import 'swiper/css/pagination';
+    import 'swiper/css/autoplay';
+     
+    // import required modules
+    import { Autoplay, Pagination } from 'swiper/modules';
+     
+    import testimonials from './data';
+    import Testimonial from './Testimonial';
+    import './testimonials.css';
 
 
 const Testimonials = () => {
