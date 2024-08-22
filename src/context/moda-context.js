c479f86e1eb1a7308b6597b21ab5438731704d69
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-08-22 14:42:01 -0300

    Update moda-context.js

--- src/context/moda-context.js
+++ src/context/moda-context.js
@@ -4,7 +4,16 @@ const ModalContext = createContext();
 
 export const ModalProvider = ({children}) => {
 
-    const useState(false);
-    return <ModalContext.Provider>{children}</ModalContext.Provider>
+    const [showModal, setShowModal] = useState(false);
+
+    const showModalHandler = () => {
+        setShowModal(true);
+    }
+
+    const closeModalHandler = () => {
+        setShowModal(false);
+    }
+
+    return <ModalContext.Provider value={{showModal, showModalHandler, closeModalHandler}}>{children}</ModalContext.Provider>
 
 }
