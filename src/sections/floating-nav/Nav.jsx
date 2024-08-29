a77a99126374b9df65ef00c55199ef426cff0054
Patrik Teixeira Saraiva <88407008+Patrikts@users.noreply.github.com>
2024-08-29 13:48:04 -0300

    Update Nav.jsx

--- src/sections/floating-nav/Nav.jsx
+++ src/sections/floating-nav/Nav.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 
-const Nav = () => {
+const Nav = ({className, item}) => {
   return (
-    <div>Nav</div>
-  )
+        <li className={className}><a href={item.link}>{item.icon}</a></li>
+    )
 }
 
 export default Nav
